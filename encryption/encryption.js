3a8bf000fa8dc0ab754884a050c9edbd0a4ed811
Dhogan06 <68254682+Dhogan06@users.noreply.github.com>
2024-04-30 15:46:46 -0600

    t

--- encryption/encryption.js
+++ encryption/encryption.js
@@ -150,21 +150,21 @@ var text = "Hello, World!";
 let key2 = cipher.generateKey();
 
 for (let index = 0; index < 10; index++) {
-    var text = cipher.scramble(text, key2);
-    var text = cipher.encrypt(text, 6);
-    var text = cipher.reverse(text);
-    var text = cipher.base64Encode(text);
+    text = cipher.scramble(text, key2);
+    text = cipher.encrypt(text, 6);
+    text = cipher.reverse(text);
+    text = cipher.base64Encode(text);
 }
 
 
 console.log(text);
 
-var text2;
+var text2 = text;
 for (let index = 0; index < 10; index++) {
-    var text2 = cipher.base64Decode(text);
-    var text2 = cipher.reverse(text2);
-    var text2 = cipher.decrypt(text2, 6);
-    var text2 = cipher.unscramble(text2, key2);
+    text2 = cipher.base64Decode(text2);
+    text2 = cipher.reverse(text2);
+    text2 = cipher.decrypt(text2, 6);
+    text2 = cipher.unscramble(text2, key2);
 }
 
 
